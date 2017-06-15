f2457eb39b727ab351b560b36cddc2b01bcefc73
Deugoue Kemayou Dieudonne Fredy <fredy.rayan@gmail.com>
2017-06-15 08:15:15 +0100

    changement

--- index.js
+++ index.js
@@ -89,8 +89,8 @@ function sendGenericMessage(sender) {
             "payload": {
                 "template_type": "generic",
                 "elements": [{
-                    "title": "Si vous souhaitez suivre",
-                    "subtitle": "social network",
+                    "title": "Bienvenue sur le bot kapkacu",
+                    "subtitle": "kapkacu paiement",
                     "image_url": "https://raw.githubusercontent.com/mlaidouni/FacebookBot/master/chatbot-facebook.jpg",
                     "buttons": [{
                         "type": "web_url",
@@ -105,41 +105,7 @@ function sendGenericMessage(sender) {
                         "url": "https://twitter.com/elmehdimobi",
                         "title": "Me Suivre sur Twitter"
                     }],
-                }, {
-                    "title": "Choses à savoir créer un bot",
-                    "subtitle": "Quelques questions sur notre bot",
-                    "image_url": "https://raw.githubusercontent.com/mlaidouni/FacebookBot/master/facebook-chatbots.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "C’est quoi un bot Facebook?",
-                        "payload": "Un bot conversationnel, c’est un logiciel informatique qui réalise des conversations automatisées. Un bot Facebook, c’est la même chose, mais sur un certain réseau social",
-                    },{
-                        "type": "postback",
-                        "title": "Quelles sont les entreprises qui ont essayé les bots ?",
-                        "payload": "Il s’agit de CNN, qui vous envoie un briefing par message chaque matin, comme une newsletter, quoi. En France, il y a notamment Skyscanner, Voyages-Sncf et KLM.",
-                    }, {
-                        "type": "postback",
-                        "title": "Comment créer votre bot Facebook?",
-                        "payload": "Il existe deux façons pour créer un bot facebook, soit d'utiliser une plateforme sans codage ou le créer en codage",
-                    }],
-                },  {
-                    "title": "Les outils nécessaires",
-                    "subtitle": "pour créer Facebook Messenger Bot",
-                    "image_url": "https://raw.githubusercontent.com/mlaidouni/FacebookBot/master/Facebook%20Messanger%20Bot.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "C'est quoi Node.js",
-                        "payload": "Node.js est une plateforme de développement Javascript. Ce n'est ni un serveur ,ni un Framework, c'est juste le langage Javascript avec des bibliothèques permettant de réaliser des actions comme écrire sur la sortie standard, ouvrir/fermer des connections réseaux ou encore créer un fichier.",
-                    },{
-                        "type": "postback",
-                        "title": "C'est quoi Heroku",
-                        "payload": "Heroku est une plateforme « Cloud » qui permet d’héberger, de développer tout type d’application. La plateforme appartient à Salesforce, gage de sécurité, de confidentialité et de haute performance.",
-                    }, {
-                        "type": "postback",
-                        "title": "C'est quoi GitHub",
-                        "payload": "GitHub est un site où n’importe qui peut déposer son projet web. Qu'il s'agit de quelques lignes de code pour une page modeste, ou d’une grosse application, les amateurs et professionnels ouvrent des comptes sur GitHub pour soumettre leur travail à l’appréciation de tous.",
-                    }],
-                }]  
+                }, ]  
             } 
         }
     }
