28107d58fb18aa1b50a0851637595bd8a4dd0e9f
Deugoue Kemayou Dieudonne Fredy <fredy.rayan@gmail.com>
2020-01-26 16:43:28 +0100

    Update index.js
    nooooo

--- index.js
+++ index.js
@@ -1,70 +1,19 @@
-// El Mehdi LAIDOUNI
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
-var app = express()
-var server = require('http').Server(app)
-var io = require('socket.io')(server)
+server.listen(80);
+// WARNING: app.listen(80) will NOT work here!
 
-var PORT = app.get('port')
-
-app.set('port', (process.env.PORT || 5000))
-// Process urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
-
-// Process json
-app.use(bodyParser.json())
-
-// Index route
 app.get('/', function (req, res) {
-    res.send('Le PORT est LE =>'+PORT)
-})
-
-
-app.listen(PORT, function() {
-    console.log('running on port', app.get('port'))
-})
-
-
-
-io.on('connection', function (socket) {
-    socket.on('onLogin', (user) => {
-      user.socket = socket.id; 
-      users[socket.id] = user; 
-      io.sockets.emit('newuser',users)
-    });
-
-
-    socket.on('onWhriting', (user_to) => {
-      for (var i = users.length - 1; i >= 0; i--) {
-        if(users[i].user.id == user_to.id){
-          user.socket = socket.id; 
-          users[socket.id] = user; 
-          io.to(user[i].socket).emit('ImOnWhriting');
-          break
-        }
-      }
-
-    });
-
-    socket.on('onSendMessage', (user_to,message) => {
-      for (var i = users.length - 1; i >= 0; i--) {
-        if(users[i].user.id == user_to.id){
-          user.socket = socket.id; 
-          users[socket.id] = user; 
-          io.to(user[i].socket).emit('IHaveSendMessage',({body:message}));
-          break
-        }
-      }
-
-    });
-
-    socket.on('disconnect', () => {
-        delete users[socket.id]
-        io.sockets.emit('userDisconnected',users)
-    });
+  res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
 
 
